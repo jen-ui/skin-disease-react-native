9ab630abf7fc47e6a73b0c54138c116bb2fd322f
Jenisha Khulal <jenishakhulal@gmail.com>
2024-06-12 15:17:32 +0545

    interface improved

--- components/pickImage.tsx
+++ components/pickImage.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
-import { Button, Image, View, StyleSheet,Text, Alert } from "react-native";
+import React ,{ useState } from "react";
+import {
+  View,
+  Button,
+  Alert,
+  Image,
+  Text,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { Picker } from "@react-native-picker/picker";
-import * as FileSystem from "expo-file-system";
-
+import * as FileSystem from "expo-file-system"
+import pako from "pako"
 
 
 import { NativeWindStyleSheet } from "nativewind";
 import { red } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
+import { FullWindowOverlay } from "react-native-screens";
 
 
 
@@ -15,21 +24,21 @@ import { red } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 
 export default function PickImage() {
-  const [image, setImage] = useState("")
+  const [image, setImage] = useState("");
   const [selectedModel, setSelectedModel] = useState("");
-  let formData = new FormData();
+  const [prediction, setPrediction] = useState("");
   const [base64String, setBase64String] = useState("");
 
- 
-  let pickImageButtonLabel = image ? "cancel image" : "Pick an image from camera roll";
-
+  let pickImageButtonLabel = image
+    ? "cancel image"
+    : "Pick an image from camera roll";
 
   const sendStringsToServer = async () => {
     try {
       console.log("whats going on");
-      
+
       const response = await fetch(
-        "http://192.168.1.66:5000//process_strings",
+        "http://172.18.161.171:5000//process_strings",
         {
           method: "POST",
           headers: {
@@ -42,14 +51,16 @@ export default function PickImage() {
         }
       );
       console.log("above data");
-      
+
       const data = await response.json();
+
+      if (data) {
+        setBase64String("");
+        setPrediction(data["prediction"]);
+      }
       console.log("below data");
-      
+
       console.log(data);
-      
-      
-      
     } catch (error) {
       console.error("Error sending strings to server:", error);
     }
@@ -57,147 +68,166 @@ export default function PickImage() {
 
   const pickImage = async () => {
     if (!image) {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+      // No permissions request is necessary for launching the image library
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
 
-    if (result.assets) {
-      console.log(result.assets[0].fileName);
-      console.log(typeof(result.assets[0]));
-      console.log(result.assets[0]);
-    }
+      // if (result.assets) {
+      //   console.log(result.assets[0].fileName);
+      //   console.log(typeof(result.assets[0]));
+      //   console.log(result.assets[0]);
+      // }
 
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
       }
-      
-   
+    } else {
+      setImage("");
+      setPrediction("");
     }
-    else {
-      setImage("")
-    }
-    
   };
 
+ 
 
-  const uint8ArrayToBase64 = (uint8Array:any) => {
-    let binaryString = "";
-    const chunkSize = 0x8000; // Arbitrary chunk size to avoid maximum call stack size exceeded error
-    for (let i = 0; i < uint8Array.length; i += chunkSize) {
-      binaryString += String.fromCharCode.apply(
-        null,
-        uint8Array.subarray(i, i + chunkSize)
-      );
-    }
-    return btoa(binaryString);
-  };
+  const convertToBase64 = async () => {
+    try {
+      const fileInfo = await FileSystem.getInfoAsync(image);
+      if (fileInfo.exists) {
+        const base64 = await FileSystem.readAsStringAsync(image, {
+          encoding: FileSystem.EncodingType.Base64,
+        });
 
-   const convertToBase64 = async () => {
-     try {
-       const fileInfo = await FileSystem.getInfoAsync(image);
-       if (fileInfo.exists) {
-         const base64 = await FileSystem.readAsStringAsync(image, {
-           encoding: FileSystem.EncodingType.Base64,
-         });
-
-        //  //compressing the base64
-        //  const binaryString = atob(base64);
-        //  const binaryArray = Uint8Array.from(binaryString, (char) =>
-        //    char.charCodeAt(0)
-        //  );
-        //  const compressed = pako.deflate(binaryArray);
-        //  const compressedBase64String = uint8ArrayToBase64(compressed);
-        //  console.log("before setting");
-         
-         setBase64String(base64);
-         console.log("after setting");
-         console.log(base64);
-         
-         
-         
-         
-         
-       }
        
-       else {
-         console.error("File does not exist:", image);
-       }
-
-     }
-     
-     catch (error) {
-       console.error("Error converting to base64:", error);
-     }
-   };
+        setBase64String(base64);
+        console.log("after setting");
+        console.log(base64);
+      } else {
+        console.error("File does not exist:", image);
+      }
+    } catch (error) {
+      console.error("Error converting to base64:", error);
+    }
+  };
 
   return (
-    <View  >
-      <Picker
-        selectedValue={selectedModel}
-        placeholder="Select the algorithm to use"
-        onValueChange={(itemValue, itemIndex) => setSelectedModel(itemValue)
-        }
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/images/bg-skin.jpg")}
+        style={styles.background}
       >
-        <Picker.Item label="ResNext" value="resnext" />
-        <Picker.Item label="Inception v4" value="inceptionv4" />
-        <Picker.Item label="Inception v3" value="inceptionv3" />
-        <Picker.Item label="Yolo v8" value="yolov8" />
-        <Picker.Item label="None" value="" />
-      </Picker>
-      <Button title={pickImageButtonLabel} onPress={pickImage} color={'green'} />
+        <View style={styles.body}>
+          <Text style={{ fontSize: 15, fontWeight:"bold" }}>Select the algorithm to use</Text>
+          <Picker
+            selectedValue={selectedModel}
+            placeholder="Select the algorithm to use"
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedModel(itemValue)
+            }
+            dropdownIconColor={"blue"}
+         
+            style={styles.picker}
+          >
+            <Picker.Item label="ResNext" value="resnext" />
+            <Picker.Item label="Inception v4" value="inceptionv4" />
+            <Picker.Item label="Inception v3" value="inceptionv3" />
+            <Picker.Item label="Yolo v8" value="yolov8" />
+            <Picker.Item label="Cancel" value="" />
+            <Picker.Item label="None" value="" />
+          </Picker>
 
-      
-        
-      {image && <Image source={{ uri: image }} />}
-      
+          {image && <Image source={{ uri: image }} style={styles.image} />}
+          {prediction && <Text style={styles.text}>{prediction}</Text>}
+          <View style={styles.buttonContainer}>
+            <Button
+              title={pickImageButtonLabel}
+              onPress={pickImage}
+              color={""}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Find the result"
+              onPress={() => {
+                console.log("submit pressed");
 
-      <Button title="submit" onPress={() => {
-        console.log("submit pressed");
-        console.log(!image);
-        console.log(!selectedModel);
-        
-        
-        
-        if (!image) {
-          Alert.alert("No Image", "Please upload the image first", [
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel Pressed"),
-              style: "cancel",
-            },
-            { text: "OK", onPress: () => console.log("OK Pressed") },
-          ]);
-        }
-          if (!selectedModel) {
-            Alert.alert("No Model", "Please select the model", [
-              {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel",
-              },
-              { text: "OK", onPress: () => console.log("OK Pressed") },
-            ]);
-        }
-        if (image && selectedModel) {
-          console.log("submit button pressed");
-          // convertToBase64();
-          console.log(base64String);
-          
-          if (base64String) {
-            console.log("base 64 obtained");
-            sendStringsToServer();
-          }
-          
-        }
-        
-        
-        
-      }}></Button>
+                if (!image) {
+                  Alert.alert("No Image", "Please upload the image first", [
+                    {
+                      text: "Cancel",
+                      onPress: () => console.log("Cancel Pressed"),
+                      style: "cancel",
+                    },
+                    { text: "OK", onPress: () => console.log("OK Pressed") },
+                  ]);
+                }
+                if (!selectedModel) {
+                  Alert.alert("No Model", "Please select the model", [
+                    {
+                      text: "Cancel",
+                      onPress: () => console.log("Cancel Pressed"),
+                      style: "cancel",
+                    },
+                    { text: "OK", onPress: () => console.log("OK Pressed") },
+                  ]);
+                }
+                if (image && selectedModel) {
+                  console.log("submit button pressed");
+                  convertToBase64();
+                  console.log(base64String);
+
+                  if (base64String) {
+                    console.log("base 64 obtained");
+                    sendStringsToServer();
+                  }
+                }
+              }}
+            />
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+
+    backgroundColor: "lightgrey",
+  },
+  background: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "cover",
+  },
+  body: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 60,
+  },
+  picker: {
+    marginBottom: 20,
+    borderWidth: 10,
+    
+    fontSize: 10,
+    
+  },
+  buttonContainer: {
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  text: {
+    textAlign: "center",
+    marginBottom: 20,
+  },
+});
